3589fb3884003a6148ca37cfaa89c9283ffcdf29
Rafael Rossi <rafaelzrossibr@gmail.com>
2020-04-30 17:30:30 -0300

    asd

--- src/server.js
+++ src/server.js
@@ -21,6 +21,29 @@ io = socketio(server, { origins: '*:*' });
 //   cookie: false
 // });
 
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+
+app.use(allowCrossDomain);
+
+
+
+
+
+
+
+
 
 io.on('connect', socket => {
   const { type } = socket.handshake.query;
