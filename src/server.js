711953f30845ac75ff922edcedffe392dcf894aa
Rafael Rossi <rafaelzrossibr@gmail.com>
2020-04-30 15:33:36 -0300

    FIRST COMMIT

---
+++ src/server.js
@@ -0,0 +1,50 @@
+const io = require('socket.io')({
+  serveClient: false,
+});
+
+const server = require('http').createServer();
+
+io.attach(server, {
+  pingInterval: 10000,
+  pingTimeout: 5000,
+  cookie: false
+});
+
+server.listen(process.env.PORT || 3333, "0.0.0.0");
+
+// const connected = [];
+const connections = [];
+
+io.on('connect', socket => {
+  const { type } = socket.handshake.query;
+
+  console.log(type, socket.id);
+
+  socket.emit('connected', socket.id);
+  
+  socket.on('attach', target => {
+    socket.to(target).emit('attach', socket.id);
+  })
+
+  socket.on('mouseMove', ({target, coordinates}) => {
+    if(target){
+      //console.log(`to: ${target} \nEmit: ${param}`);
+      socket.to(target).emit('mouseMove', coordinates);
+    }
+  })
+
+  socket.on('mouseClick', (target) => {
+    if(target){
+      //console.log(`to: ${target} \nEmit: ${param}`);
+      socket.to(target).emit('mouseClick', socket.id);
+    }
+  })
+  socket.on('keyPress', ({target, key}) => {
+    if(target){
+      //console.log(`to: ${target} \nEmit: ${param}`);
+      socket.to(target).emit('keyPress', key);
+    }
+  })
+
+
+});
